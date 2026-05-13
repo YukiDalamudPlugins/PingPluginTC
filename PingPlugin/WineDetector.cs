d8fd7803d447baf3b6300d04641fc1eeb0181097
Yinah <98339032+yfthbr@users.noreply.github.com>
2026-05-13 19:44:53 +0300

    use dalamud's wine detector (#69)

--- PingPlugin/WineDetector.cs
+++ PingPlugin/WineDetector.cs
@@ -1,25 +1,13 @@
-﻿using System.Runtime.InteropServices;
+﻿using Dalamud.Utility;
 
 namespace PingPlugin;
 
-public static partial class WineDetector
+public static class WineDetector
 {
+    private static bool? isWine;
+
     public static bool IsWINE()
     {
-        var ntdll = GetModuleHandle("ntdll.dll");
-        if (ntdll == nint.Zero)
-        {
-            return false;
-        }
-
-        var wineGetVersion = GetProcAddress(ntdll, "wine_get_version");
-        return wineGetVersion != nint.Zero;
+        return isWine ??= Util.IsWine();
     }
-
-    [LibraryImport("kernel32.dll", EntryPoint = "GetModuleHandleW", SetLastError = true,
-        StringMarshalling = StringMarshalling.Utf16)]
-    private static partial nint GetModuleHandle(string lpModuleName);
-
-    [LibraryImport("kernel32.dll", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
-    private static partial nint GetProcAddress(nint hModule, string procName);
 }
